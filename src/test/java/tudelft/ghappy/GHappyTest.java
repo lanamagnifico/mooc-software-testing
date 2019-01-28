94e81f6e8af91506279ed92571529ebf16a3ed8f
lanamagnifico <svetlana.magnifico@gmail.com>
2019-01-28 15:32:45 +0100

    Add GHappy test

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,35 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    @Test
+    public void testHappy(){
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("xggy");
+        Assertions.assertEquals(result, true);
+    }
+
+    @Test
+    public void testUnhappy(){
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("xgy");
+        Assertions.assertEquals(result, false);
+    }
+
+    @Test
+    public void testUnhappyCombi(){
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("xgqyaag");
+        Assertions.assertEquals(result, false);
+    }
+
+    @Test
+    public void testOnlyG(){
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("gg");
+        Assertions.assertEquals(result, true);
+    }
 }
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,21 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+
+    @Test
+    public void mirrorAbXYZ(){
+        Mirror mirror = new Mirror();
+        String abXYZba = mirror.mirrorEnds("abXYZba");
+        Assertions.assertEquals("ab",abXYZba);
+    }
+
+    @Test
+    public void testA(){
+        Mirror mirror = new Mirror();
+        String a = mirror.mirrorEnds("abca");
+        Assertions.assertEquals("a",a);
+    }
 }
