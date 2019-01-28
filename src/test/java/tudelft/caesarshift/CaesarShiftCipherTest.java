17243b8678da5e8443314ff65ca0f8bb8645a322
lanamagnifico <svetlana.magnifico@gmail.com>
2019-01-28 15:49:33 +0100

    Add Caeser Shift Cipher test

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,29 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CaesarShiftCipherTest {
+
+    @Test
+    public void testAbcPositive(){
+        CaesarShiftCipher caesarShiftCipher = new CaesarShiftCipher();
+        String result = caesarShiftCipher.CaesarShiftCipher("abc",3);
+        Assertions.assertEquals("def",result);
+    }
+
+    @Test
+    public void testAbcNegative(){
+        CaesarShiftCipher caesarShiftCipher = new CaesarShiftCipher();
+        String result = caesarShiftCipher.CaesarShiftCipher("abc",-3);
+        Assertions.assertEquals("xyz",result);
+    }
+
+    @Test
+    public void testLastSymbols(){
+        CaesarShiftCipher caesarShiftCipher = new CaesarShiftCipher();
+        String result = caesarShiftCipher.CaesarShiftCipher("xyz",3);
+        Assertions.assertEquals("abc",result);
+    }
 
 }
